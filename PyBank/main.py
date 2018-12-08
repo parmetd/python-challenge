f701b9b3cfcb34bbbf0b96e53f0eb653e229f632
ChaosParadigm@EternalFlame <parmetd@fastmail.fm>
2018-12-07 18:11:34 -0600

    import csv done

---
+++ PyBank/main.py
@@ -0,0 +1,23 @@
+#PyBank
+
+import os
+import pandas as pd 
+import csv
+#Dependencies
+
+csv_bank = os.path.join('Resources', 'budget_data.csv')
+
+with open(csv_bank, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
